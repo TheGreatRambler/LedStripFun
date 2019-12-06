e7cc6ad2ff2ec743f86397481a19ef3c99917a5f
TheGreatRambler <aehart684351@gmail.com>
2019-12-06 05:25:10 +0000

    Added main

---
+++ source/main.cpp
@@ -0,0 +1,92 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <getopt.h>
+
+extern "C" {
+#include "rpi_ws281x/clk.h"
+#include "rpi_ws281x/gpio.h"
+#include "rpi_ws281x/dma.h"
+#include "rpi_ws281x/pwm.h"
+#include "rpi_ws281x/ws2811.h"
+}
+
+const int ledStringLength = 150;
+
+// Struct defining important stuff
+ws2811_t ledstring = {
+    .freq = WS2811_TARGET_FREQ,
+    .dmanum = 10,
+    .channel = {
+        [0] = {
+            .gpionum = 18,
+            .count = ledStringLength,
+            .invert = 0,
+            .brightness = 255, // 0 to 255 brightness
+            .strip_type = WS2811_STRIP_GRB,
+        },
+        [1] = {
+            .gpionum = 0,
+            .count = 0,
+            .invert = 0,
+            .brightness = 0,
+        },
+    },
+};
+
+void clearLedString() {
+    for (int i = 0; i < ledStringLength; i++) {
+        ledstring.channel[0].leds[i] = 0;
+    }
+}
+
+static bool running = true;
+
+static void ctrl_c_handler(int signum) {
+	(void)(signum);
+    running = false;
+}
+
+int main(int argc, char *argv[]) {
+    ws2811_return_t ret;
+
+    // Handle Ctrl C
+    struct sigaction sa = {
+        .sa_handler = ctrl_c_handler,
+    };
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+
+    if ((ret = ws2811_init(&ledstring)) != WS2811_SUCCESS) {
+        fprintf(stderr, "ws2811_init failed: %s\n", ws2811_get_return_t_str(ret));
+        return ret;
+    }
+
+    while (running) {
+        //matrix_raise();
+        //matrix_bottom();
+        //matrix_render();
+
+        if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS) {
+            fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
+            break;
+        }
+
+        // 15 frames /sec
+        usleep(1000000 / 15);
+    }
+
+    clearLedString();
+    ws2811_render(&ledstring);
+    ws2811_fini(&ledstring);
+
+    return ret;
+}
