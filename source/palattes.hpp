ad9c5efa32c1d2060cf8553c778643a056743b44
TheGreatRambler <aehart684351@gmail.com>
2019-12-08 04:42:04 +0000

    Formatted something
    Signed-off-by: TheGreatRambler <aehart684351@gmail.com>

--- source/palattes.hpp
+++ source/palattes.hpp
@@ -1,12 +1,12 @@
 #include <rpi_ws281x/clk.h>
-#include <rpi_ws281x/gpio.h>
 #include <rpi_ws281x/dma.h>
+#include <rpi_ws281x/gpio.h>
 #include <rpi_ws281x/pwm.h>
 #include <rpi_ws281x/ws2811.h>
 
 // Check https://gka.github.io/palettes
 ws2811_led_t dotcolors[] = {
-    0x00ffff,  // lightblue
+    0x00ffff, // lightblue
 };
 
 const uint16_t palatteSize = sizeof(dotcolors) / sizeof(dotcolors[0]);
@@ -14,5 +14,5 @@ const uint16_t palatteSize = sizeof(dotcolors) / sizeof(dotcolors[0]);
 // Loop over the array to map the values correctly
 // Simply bitshift to the right to make the W value zero
 for (int i = 0; i < palatteSize; i++) {
-    dotcolors[i] = dotcolors[i] >> 2;
+  dotcolors[i] = dotcolors[i] >> 2;
 }
