f266c4843b8c70601e39312ff3d9e248dbaecfed
TheGreatRambler <aehart684351@gmail.com>
2019-12-10 03:17:23 +0000

    Changed to primary colors
    Signed-off-by: TheGreatRambler <aehart684351@gmail.com>

--- source/palattes.hpp
+++ source/palattes.hpp
@@ -7,9 +7,6 @@
 #include <rpi_ws281x/ws2811.h>
 
 // Check https://gka.github.io/palettes
-ws2811_led_t dotcolors[] = {0xff0000, 0xf52700, 0xec3800, 0xe24400, 0xd84d00,
-                            0xce5500, 0xc45b00, 0xba6100, 0xb06500, 0xa66a00,
-                            0x9b6d00, 0x917100, 0x867400, 0x7a7600, 0x6e7800,
-                            0x617a00, 0x537c00, 0x437e00, 0x2d7f00, 0x008000};
+ws2811_led_t dotcolors[] = {0xff0000, 0x00ff00, 0x0000ff};
 
 const uint16_t palatteSize = sizeof(dotcolors) / sizeof(dotcolors[0]);
