0917987f5891c3eb2d940cec5300c85846e717dc
TheGreatRambler <aehart684351@gmail.com>
2019-12-10 03:05:25 +0000

    Hmm
    Signed-off-by: TheGreatRambler <aehart684351@gmail.com>

--- source/palattes.hpp
+++ source/palattes.hpp
@@ -7,6 +7,9 @@
 #include <rpi_ws281x/ws2811.h>
 
 // Check https://gka.github.io/palettes
-ws2811_led_t dotcolors[] = {0xce5500};
+ws2811_led_t dotcolors[] = {0xff0000, 0xf52700, 0xec3800, 0xe24400, 0xd84d00,
+                            0xce5500, 0xc45b00, 0xba6100, 0xb06500, 0xa66a00,
+                            0x9b6d00, 0x917100, 0x867400, 0x7a7600, 0x6e7800,
+                            0x617a00, 0x537c00, 0x437e00, 0x2d7f00, 0x008000};
 
 const uint16_t palatteSize = sizeof(dotcolors) / sizeof(dotcolors[0]);
